34656997aa262149f4585815dfad2679fd33a9b8
Hope4orcas <124528587+Hope4orcas@users.noreply.github.com>
2023-03-14 18:07:06 +0100

    Update hellooo.py

--- hellooo.py
+++ hellooo.py
@@ -2,8 +2,10 @@ import streamlit as st
 import requests
 import json
 
+sub=st.text_input("subreddit")
+
 if st.button("Generate meme"):
-  url="https://meme-api.com/gimme/cats"
+  url="https://meme-api.com/gimme/"+sub
   response = requests.get(url)
   data = json.loads(response.text)
   meme = data["url"]
